4c53b55d21df02e5a0e44633e21b80727b821129
DiegoLopezGlz <diego_lopezglz@hotmail.com>
2024-03-16 22:07:50 -0600

    Este es mi primer commit

---
+++ src/main/java/mx/com/openpay/marvel/MarvelApplication.java
@@ -0,0 +1,13 @@
+package mx.com.openpay.marvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelApplication.class, args);
+	}
+
+}
---
+++ src/main/java/mx/com/openpay/marvel/Prueba.java
@@ -0,0 +1,13 @@
+package mx.com.openpay.marvel;
+
+public class Prueba {
+
+    /**
+     * Método para generar el mensaje de prueba
+     * @param mensaje que es el mensaje que se va a concatenar y retornar
+     * @return el mensaje concatenado qie valida la prueba
+     */
+    public String getMensaje(String mensaje){
+        return "El mensaje es: " + mensaje;
+    }
+}
